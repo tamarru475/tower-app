ff8174fc605a3c72663345cf9dad08277eba39ed
Tamarru475 <tamarrubin475@gmail.com>
2023-03-26 18:05:16 +1300

    first test try

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,11 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App/App'
+
+test('render current user greeting', () => {
+  render(<App />)
+
+  const userGreeting = screen.getByText(/welcome Tamar/i)
+
+  expect(userGreeting).toBeInTheDocument()
+})
