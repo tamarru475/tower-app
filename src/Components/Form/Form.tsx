9c057ccf3e6977e7ad8c4a585af6d76f06936d2f
Tamarru475 <tamarrubin475@gmail.com>
2023-03-26 17:43:54 +1300

    form markup form style form validation

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,1 +1,80 @@
 import React from 'react'
+import { usePaymentInputs } from 'react-payment-inputs'
+
+const Form: React.FC = () => {
+  const { meta, getCardNumberProps, getExpiryDateProps, getCVCProps } =
+    usePaymentInputs()
+  const [values, setValues] = React.useState({
+    cardNumber: '',
+    expiryDate: '',
+    cvc: '',
+  })
+
+  const isButtonDisabled = !meta.isTouched || (meta.isTouched && meta.error)
+  const disabledButtonClass = isButtonDisabled ? 'form__btn_disabled' : ''
+
+  const handleChange = (event: Event) => {
+    const target = event.target as HTMLInputElement
+    const name = target?.name
+    const value = target?.value
+    setValues({ ...values, [name]: value })
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    console.log(values)
+    setValues({
+      cardNumber: '',
+      expiryDate: '',
+      cvc: '',
+    })
+  }
+
+  return (
+    <form className='form' name='form' onSubmit={handleSubmit}>
+      <div className='form__fieldset'>
+        <label className='form__lable'>Card details</label>
+        <input
+          className={`form__input `}
+          {...getCardNumberProps({ onChange: handleChange })}
+          value={values.cardNumber}
+          required
+        />
+        {meta.isTouched && meta.error && <span>Error: {meta.error}</span>}
+      </div>
+      <div className='form__inputs-group'>
+        <div className='form__fieldset'>
+          <label className='form__lable'>Expiry</label>
+          <input
+            className={`form__input form__input-small `}
+            {...getExpiryDateProps({ onChange: handleChange })}
+            value={values.expiryDate}
+            required
+          />
+          {meta.isTouched && meta.error && <span>Error: {meta.error}</span>}
+        </div>
+        <div className='form__fieldset'>
+          <label className='form__lable'>CVC</label>
+          <input
+            className={`form__input form__input-small `}
+            {...getCVCProps({ onChange: handleChange })}
+            value={values.cvc}
+            required
+          />
+          {meta.isTouched && meta.error && <span>Error: {meta.error}</span>}
+        </div>
+      </div>
+      <div className='form__fieldset form__btn-fieldset'>
+        <button
+          disabled={isButtonDisabled ? true : false}
+          type='submit'
+          className={`form__btn ${disabledButtonClass}`}
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default Form
--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Greeting from '../Components/Greeting/Greeting'
+import Form from '../Components/Form/Form'
 
 const Main: React.FC = () => {
   return (
     <main className='main'>
       <Greeting />
+      <Form />
     </main>
   )
 }
